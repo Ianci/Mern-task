e52dd274fdb8e65df20805c2bc61a4183726592e
ianci <ianbergia.rc@gmail.com>
2020-12-10 20:04:24 -0300

    agregado username a la nav

--- src/components/main/TaskScreen.js
+++ src/components/main/TaskScreen.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
 import { MainContent } from './MainContent'
 import { Sidebar } from './Sidebar'
 import { UserNav } from './UserNav'
+import { authContext } from '../../context/auth/authContext'
 
 export const TaskScreen = () => {
+    
+    const context = useContext(authContext)
+    const { authenticatedUser, taskScreenFunction } = context
+
+    useEffect(() => {
+        authenticatedUser()
+        taskScreenFunction()
+    }, [])
+
     return (
         <>
         
--- src/components/main/UserNav.js
+++ src/components/main/UserNav.js
@@ -1,10 +1,14 @@
-import React from 'react'
-
+import React, { useContext} from 'react'
+import { authContext } from '../../context/auth/authContext'
 export const UserNav = () => {
+
+    const { user, logOutUser } = useContext(authContext)
+    const { name } = !!user && user
+
     return (
         <div className="taskScreen__userNav">
-            <p className="taskScreen__paragraph taskScreen__hello-user">Hola Ianci</p>
-            <button type="button" className="register__btn-submit taskScreen__btn-logout">Logout</button>
+            <p className="taskScreen__paragraph taskScreen__hello-user">Hola {name}</p>
+            <button type="button" className="register__btn-submit taskScreen__btn-logout" onClick={logOutUser}>Logout</button>
             
         </div>
     )
